c1d694ba97b2989db2a5b24a9b7defe7f29d4dc6
janaa-wael <janawael119@gmail.com>
2025-08-30 17:21:13 +0300

    Implementing Flight Class

---
+++ Flight.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Flight.hpp"
+using namespace std;
+
+bool Flight::checkAvailability()
+{
+    return availableSeats > 0;
+}
+
+void Flight::reserveSeat()
+{
+    if (checkAvailability()) {
+        availableSeats--;
+    } else {
+        cout << "No available seats to reserve." << endl;
+    }
+}
+void Flight::cancelSeat()
+{
+    availableSeats++;
+}
+
+void Flight::displayFlightInfo()
+{
+    cout << "Flight Number: " << id << endl;
+    cout << "Origin: " << source << endl;
+    cout << "Destination: " << destination << endl;
+    cout << "Departure Time: " << departureTime << endl;
+    cout << "Arrival Time: " << arrivalTime << endl;
+    cout << "Available Seats: " << availableSeats << endl;
+}
